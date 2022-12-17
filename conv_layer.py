6db07dc0c5455769d20527a024bbf8edbb0e7efa
Vaibhav Ramola <ramolavaibhav92002@gmail.com>
2022-12-17 22:28:30 +0530

    backpropagation for cnn done

--- conv_layer.py
+++ conv_layer.py
@@ -40,7 +40,6 @@ class Conv(Layer):
         self.input = input
         self.input_shape = input.shape
         self.output = np.copy(self.bias)
-
         '''
         forward propagation is the cross-correlation between 
         each input channel of the image
@@ -59,10 +58,35 @@ class Conv(Layer):
         return self.output
 
     
-    def backward(self, output_grad, learning_rate):
+    def backward(self, output_grads, learning_rate):
+        kernel_grads = np.zeros(self.kernel.shape)
+        input_grads = np.zeros(self.input_shape)
         
+        '''
+        Given:
+            Gradient of loss w.r.t. output as output_grads
+        To do the backward propagation we need 3 things:
+            1.  Gradient of loss w.r.t. kernel(or kernel weights) to update the kernels
+            2.  Gradient of loss w.r.t bias(or bias wieghts)  to update bias
+            3.  Gradient of loss w.r.t. input so that it can be backward propagated as output_grads for previous layer
+        '''
+        for out_channel in range(self.out_channels):
+            for in_channel in range(self.in_channels):
+                kernel_grads[out_channel][in_channel] = signal.correlate2d(self.input[in_channel], output_grads[out_channel], mode='valid')
+                # gradient of loss w.r.t. kernels is the valid correlation of input with output_grads
+                input_grads[in_channel] += signal.convolve2d(output_grads[out_channel], self.kernel[out_channel][in_channel], mode='full')
+                # gradient of loss w.r.t. input is the full convolution of output_grads and kernels
+                # To know more about signal.convolve2d, look at
+                # Link : https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.convolve2d.html
 
-        return super().backward(output_grad, learning_rate)
+        # gradient of loss w.r.t. bias is the same as the output_grads
+
+        # Weight update
+        self.kernel -= learning_rate * kernel_grads
+        self.bias -= learning_rate * output_grads
+
+        # backpropagation
+        return input_grads 
         
         
-        return super().forward(input)
+        
